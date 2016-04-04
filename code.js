9e607e36864c62617a325d6e784101a2d5eff6a7
pacosta613 <pedro.acosta613@gmail.com>
2016-04-04 15:39:44 -0400

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(icecream){
+  return "I love " + icecream;
+}
+
+function shouting(string){
+  return string.toUpperCase();
+}
+
+function roundDown(number){
+  return Math.floor(number);
+}
+
+function theTruth(){
+  return true;
+}
